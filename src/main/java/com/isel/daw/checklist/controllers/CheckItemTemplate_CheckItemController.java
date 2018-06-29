45658846b86fa22839b4ec7a674aef2acc8592e9
BernardoPoiares <39100@alunos.isel.ipl.pt>
2018-06-29 16:30:38 +0100

    Added new service for services of checkitem that needs checkitemtemplate services

---
+++ src/main/java/com/isel/daw/checklist/controllers/CheckItemTemplate_CheckItemController.java
@@ -0,0 +1,71 @@
+package com.isel.daw.checklist.controllers;
+
+import com.isel.daw.checklist.RequiresAuthentication;
+import com.isel.daw.checklist.ServiceResponse;
+import com.isel.daw.checklist.model.DataBaseDTOs.CheckItem;
+import com.isel.daw.checklist.model.RequestsDTO.CheckItemRequestDto;
+import com.isel.daw.checklist.model.ResponseBuilder;
+import com.isel.daw.checklist.model.SirenBuilders.CheckItemSirenBuilder;
+import com.isel.daw.checklist.services.CheckItemService;
+import com.isel.daw.checklist.services.CheckItemTemplate_CheckItemService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/checkItemTemplate_checkitem")
+public class CheckItemTemplate_CheckItemController {
+
+    private final CheckItemTemplate_CheckItemService itemtemplate_itemService;
+
+    @Autowired
+    public CheckItemTemplate_CheckItemController(CheckItemTemplate_CheckItemService itemtemplate_itemService){
+        this.itemtemplate_itemService=itemtemplate_itemService;
+    }
+
+
+        @PostMapping(path="/create", produces={"application/vnd.siren+json","application/problem+json"})
+    @RequiresAuthentication
+    public ResponseEntity<?> create(@RequestHeader(value="Authorization")String authorization, @RequestBody CheckItemRequestDto checkItemRequestDto) {
+        ServiceResponse<CheckItem> response=itemtemplate_itemService.create(authorization,checkItemRequestDto);
+        if(response.getError()!=null)
+            return ResponseBuilder.buildError(response.getError());
+        CheckItem checkItem=response.getResponse();
+        return ResponseBuilder.build(
+                CheckItemSirenBuilder.build(checkItem.getId(),
+                        checkItem.getCheckitem_itemtemplate().getName(),
+                        checkItem.getCheckitem_itemtemplate().getDescription(),
+                        checkItem.getState())
+        );
+    }
+
+    @PostMapping(path="/update", produces={"application/vnd.siren+json","application/problem+json"})
+    @RequiresAuthentication
+    public ResponseEntity<?> update(@RequestHeader(value="Authorization")String authorization, @RequestBody CheckItemRequestDto checkItemRequestDto) {
+        ServiceResponse<?> response=itemtemplate_itemService.update(authorization,checkItemRequestDto);
+        if(response.getError()!=null)
+            return ResponseBuilder.buildError(response.getError());
+        CheckItem checkItem=(CheckItem)response.getResponse();
+        return ResponseBuilder.build(
+                CheckItemSirenBuilder.build(checkItem.getId(),
+                        checkItem.getCheckitem_itemtemplate().getName(),
+                        checkItem.getCheckitem_itemtemplate().getDescription(),
+                        checkItem.getState())
+        );
+    }
+
+    @DeleteMapping(path="/{id}", produces={"application/vnd.siren+json","application/problem+json"})
+    @RequiresAuthentication
+    public ResponseEntity<?> delete(@RequestHeader(value="Authorization")String authorization, @PathVariable("id") long id) {
+        ServiceResponse<CheckItem> response=itemtemplate_itemService.delete(authorization,id);
+        if(response.getError()!=null)
+            return ResponseBuilder.buildError(response.getError());
+        CheckItem checkItem=response.getResponse();
+        return ResponseBuilder.build(
+                CheckItemSirenBuilder.build(checkItem.getId(),
+                        checkItem.getCheckitem_itemtemplate().getName(),
+                        checkItem.getCheckitem_itemtemplate().getDescription(),
+                        checkItem.getState())
+        );
+    }
+}
--- src/main/java/com/isel/daw/checklist/services/CheckItemService.java
+++ src/main/java/com/isel/daw/checklist/services/CheckItemService.java
@@ -53,103 +53,12 @@ public class CheckItemService implements Service {
     }
 
 
-    //TODO:CHANGE TO NEW SERVICE checkitemtemplate_checkitem
-    @Transactional
-    public ServiceResponse<CheckItem> create(String authorization, CheckItemRequestDto checkitem_dto){
-        Users user=userRepository.findByToken(authorization.split(" ")[1]);
-        ValidatorResponse valtrequest=CheckItemValidator.validateCkitDto(checkitem_dto);
-        if(!valtrequest.isValid)
-            return new ServiceResponse<>(null,valtrequest.problem);
-        CheckItemTemplate newitemTemplate=new CheckItemTemplate(checkitem_dto.getCheckitemtemplate().getName(),checkitem_dto.getCheckitemtemplate().getDescription(),user);
-        CheckItemTemplate saveditemtemplate=itemTemplateRepository.save(newitemTemplate);
-        if(saveditemtemplate==null)
-            return new ServiceResponse<>(null,new InternalServerProblem());
-        CheckItem newitem=new CheckItem("uncompleted",saveditemtemplate);
-        CheckItem savedcheckitem=itemRepository.save(newitem);
-        if(savedcheckitem==null) {
-            itemTemplateRepository.deleteById(saveditemtemplate.getId());
-            return new ServiceResponse<>(null, new InternalServerProblem());
-        }
-        return new ServiceResponse<>(savedcheckitem,null);
-    }
-
-
-    //TODO:CHANGE TO NEW SERVICE checkitemtemplate_checkitem
-    /*
-    @Transactional
-    public ServiceResponse<?> createByTemplate(String authorization, CheckItemTemplateRequestDto checkitemtemplate_dto){
-        Users user=userRepository.findByToken(authorization.split(" ")[1]);
-        ValidatorResponse valtrequest=CheckItemValidator.validatCkitTemp(checkitemtemplate_dto);
-        if(!valtrequest.isValid)
-            return new ServiceResponse<>(null,valtrequest.problem);
-        ServiceResponse<CheckItemTemplate> service_res=checkItemTemplateService.getById(authorization,checkitemtemplate_dto.getId());
-        if(service_res.getError()!=null)
-            return service_res;
-        CheckItem newitem=new CheckItem("uncompleted",service_res.getResponse());
-        CheckItem savedcheckitem=itemRepository.save(newitem);
-        if(savedcheckitem==null)
-            return new ServiceResponse<>(null, new InternalServerProblem());
-        return new ServiceResponse<>(savedcheckitem,null);
-    }*/
-
-    @Transactional
     public ServiceResponse<CheckItem> createByTemplate(CheckItemTemplate checkitemtemplate){
         CheckItem newitem=new CheckItem("uncompleted",checkitemtemplate);
         CheckItem savedcheckitem=itemRepository.save(newitem);
         if(savedcheckitem==null)
             return new ServiceResponse<>(null, new InternalServerProblem());
         return new ServiceResponse<>(savedcheckitem,null);
-    }
-
-
-    //TODO:CHANGE TO NEW SERVICE checkitemtemplate_checkitem
-    @Transactional
-    public ServiceResponse<?> update(String authorization, CheckItemRequestDto checkitem_dto){
-        ServiceResponse<CheckItem> serv_resp=getItemById(authorization,checkitem_dto.getId());
-        if(serv_resp.getError()!=null)
-            return serv_resp;
-        CheckItem checkitem=serv_resp.getResponse();
-        if(checkitem_dto.getState()!=null)
-            checkitem.setState(checkitem_dto.getState());
-        CheckItemTemplateRequestDto ckit_dto=checkitem_dto.getCheckitemtemplate();
-        ValidatorResponse valtrequest= CheckItemTemplateValidator.validateUpdateRequest(ckit_dto);
-        if(!valtrequest.isValid)
-            return new ServiceResponse<>(null,valtrequest.problem);
-        CheckItemTemplate itemtemplatetoupdate=checkitem.getCheckitem_itemtemplate();
-        long numbTempuses=itemRepository.countByTemplateId(ckit_dto.getId());
-        if(numbTempuses>1) {
-            ServiceResponse<CheckItemTemplate> ckittemp_res=checkItemTemplateService.create(authorization,checkitem_dto.getCheckitemtemplate());
-            if(ckittemp_res.getError()!=null)
-                return ckittemp_res;
-            itemtemplatetoupdate=ckittemp_res.getResponse();
-            checkitem.setCheckitem_itemtemplate(itemtemplatetoupdate);
-        }
-        if(ckit_dto.getName()!=null)
-            itemtemplatetoupdate.setName(ckit_dto.getName());
-        if(ckit_dto.getDescription()!=null)
-            itemtemplatetoupdate.setDescription(ckit_dto.getDescription());
-        return new ServiceResponse<>(checkitem,null);
-    }
-
-    //TODO:CHANGE TO NEW SERVICE checkitemtemplate_checkitem
-    @Transactional
-    public ServiceResponse<CheckItem> delete(String authorization, long id){
-        ServiceResponse<CheckItem> serv_resp=getItemById(authorization,id);
-        if(serv_resp.getError()!=null)
-            return serv_resp;
-        CheckItem checkitem=serv_resp.getResponse();
-        long delt_item_res= itemRepository.deleteById(checkitem.getId());
-        if(delt_item_res==0)
-            return new ServiceResponse<>(null,new InternalServerProblem());
-        long numbTempuses=itemRepository.countByTemplateId(checkitem.getCheckitem_itemtemplate().getId());
-        if(numbTempuses==0) {
-            long delt_itemtemp_res=itemTemplateRepository.deleteById(checkitem.getCheckitem_itemtemplate().getId());
-            if(delt_itemtemp_res==0){
-                TransactionAspectSupport.currentTransactionStatus().setRollbackOnly(); //set rollback
-                return new ServiceResponse<>(null,new InternalServerProblem());
-            }
-        }
-        return new ServiceResponse<>(checkitem,null);
     }
 
 }
