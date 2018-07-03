d3ddb78449a52d4a1e426909d1115c868dfb792f
BernardoPoiares <39100@alunos.isel.ipl.pt>
2018-07-04 00:36:31 +0100

    Changed directories to separete react from httpserver.Started router on react

--- js/js/index.js
+++ js/js/index.js
@@ -1,11 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './ui/app'
+import Home from './ui/home'
+import { Link } from 'react-router-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 
 function example5 () {
   ReactDOM.render(
-    <App />,
+    <div> <header>
+    <nav>
+    <Router> 
+      <ul>
+        <li><Link to='/sadsad' component={Home}/></li>
+      </ul>
+      </Router>
+    </nav>
+  </header></div>,
     document.getElementById('root')
   )
 }
